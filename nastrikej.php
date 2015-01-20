ac683dc06278d83314a6af81f5de264282946bc5
vEnCa-X <vEnCa-X@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-20 19:20:46 +0000

    v 0.0.3
    git-svn-id: https://plugins.svn.wordpress.org/nastrikejcz/trunk@1071883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nastrikej.php
+++ nastrikej.php
@@ -4,29 +4,27 @@
   Plugin Name: Nastříkej
   Plugin URI: http://www.nastrikej.cz
   Description: Rozpis hasičských soutěží/výsledků z celé ČR
-  Version: 0.2
+  Version: 0.3
   Author: vEnCa-X
   Author URI: http://www.venca-x.cz
   License: MIT
  */
 
-class Nastrikej extends WP_Widget
+define("URL", "http://www.nastrikej.cz/json/");
+    
+class NastrikejCompetitions extends WP_Widget
 {
-
-    protected $url = "http://www.nastrikej.cz/json/";
 
     public function __construct()
     {
-        parent::__construct( false, $name = __( 'Nastříkej' ) );
+        parent::__construct( false, $name = __( 'Nastříkej - soutěže' ) );
     }
 
     public function form( $instance )
     {
-        $instance = wp_parse_args(
-                (array) $instance
-        );
+        $instance = wp_parse_args( (array) $instance );
 
-        if ( !empty( $instance['title'] ) )
+        if ( isset( $instance['title'] ) )
         {
             $title = $instance['title'];
         }
@@ -34,8 +32,8 @@ class Nastrikej extends WP_Widget
         {
             $title = __( 'Soutěže', 'wptuts-countdowner-locale' );
         }
-        
-        if ( !empty( $instance['seasonId'] ) )
+
+        if ( isset( $instance['seasonId'] ) )
         {
             $seasonId = $instance['seasonId'];
         }
@@ -44,7 +42,7 @@ class Nastrikej extends WP_Widget
             $seasonId = "";
         }
         
-        if ( !empty( $instance['teamsId'] ) )
+        if ( isset( $instance['teamsId'] ) )
         {
             $teamsId = $instance['teamsId'];
         }
@@ -54,7 +52,7 @@ class Nastrikej extends WP_Widget
         }        
 
         // Display the admin form
-        include( plugin_dir_path( __FILE__ ) . '/views/admin.php' );
+        include( plugin_dir_path( __FILE__ ) . '/views/admin-competitions.php' );
     }
 
     public function update( $new_instance, $old_instance )
@@ -62,7 +60,7 @@ class Nastrikej extends WP_Widget
         $instance = $old_instance;
 
         $instance['title'] = strip_tags( $new_instance['title'] );
-        $instance['seasonId'] = strip_tags( $new_instance['seasonId'] );       
+        $instance['seasonId'] = strip_tags( $new_instance['seasonId'] );
         $instance['teamsId'] = esc_sql( $new_instance['teamsId'] );        
         return $instance;
     }
@@ -73,51 +71,50 @@ class Nastrikej extends WP_Widget
 
         echo $before_widget;
 
-        $title = apply_filters( 'wptuts_countdowner_title', $instance['title'] );
-        $seasonId = apply_filters( 'season_id', $instance['seasonId'] );
-                
-        $url = $this->url . "competition-in-season/" . $seasonId;
+        $title = apply_filters('widget_title', $instance['title']);
+        $seasonId = $instance['seasonId'];
+
+        $url = URL . "competition-in-season/" . $seasonId;
         if( is_array( $instance['teamsId'] ) )
         {
             $url.= "?team=" . implode( "x", $instance['teamsId'] );
         }
 
         //echo "".$url."";
-                
+
         $json = file_get_contents( $url );
         $data = json_decode( $json, TRUE );
-        
-        $table = "<table id='nastrikej'>
+
+        $table = "<table class='nastrikej'>
                     <tr>
                         <th>Datum</th>
                         <th>Místo</th>
-                    </tr>";        
-                    foreach ( $data["competitions"] as $competition )
+                    </tr>";
+        foreach ( $data["competitions"] as $competition )
+        {
+            $table.="<tr>";
+            $table.="<td class='competition'>" . $competition["date"] . "&nbsp;</td><td class='competition-village'><a href='" . $competition["href"] . "'>" . $competition["village"] . "</a></td>";
+            $table.="</tr>";
+
+            if( count( $competition["results"] ) > 0 )
+            {
+                foreach( $competition["results"] as $result)
+                {
+                    $video = "";
+                    foreach( $result["youtubes"] as $youtube )
                     {
-                        $table.="<tr>";
-                            $table.="<td class='competition'>" . $competition["date"] . "&nbsp;</td><td class='competition-village'><a href='" . $competition["href"] . "'>" . $competition["village"] . "</a></td>";
-                        $table.="</tr>";
-                        
-                        if( count( $competition["results"] ) > 0 )
-                        {
-                            foreach( $competition["results"] as $result)
-                            {
-                                $video = "";
-                                foreach( $result["youtubes"] as $youtube )
-                                {
-                                    $video.= " <a href='" . $youtube . "' target='_blank' class='competition-video'><img src='http://www.nastrikej.cz/pictures/video-icon.png' alt='video'></a> ";
-                                }
-                                
-                                $table.="<tr>";
-                                    $table.="<td colspan=\"2\" class='competition-result'><small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$result["position"]}. {$result["team"]}, čas: {$result["time"]}</small>".$video."</td>";
-                                    //,{$result["position"]}.místo";
-                                $table.="</tr>";                            
-                            }
-                        }
-                        
-                    }                
+                        $video.= " <a href='" . $youtube . "' target='_blank' class='competition-video'><img src='http://www.nastrikej.cz/pictures/video-icon.png' alt='video'></a> ";
+                    }
+
+                    $table.="<tr>";
+                    $table.="<td colspan=\"2\" class='competition-result'><small>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{$result["position"]}. {$result["team"]}, čas: {$result["time"]}</small>".$video."</td>";
+                    $table.="</tr>";
+                }
+            }
+        }
+
         $table.= "</table>";
-                
+
         include( plugin_dir_path( __FILE__ ) . '/views/widget.php' );
 
         echo $after_widget;
@@ -125,6 +122,134 @@ class Nastrikej extends WP_Widget
 
 }
 
-add_action('widgets_init', create_function('', 'return register_widget("nastrikej");'));
+class NastrikejPositions extends WP_Widget
+{
+
+    public function __construct()
+    {
+        parent::__construct( false, $name = __( 'Nastříkej - pořadí' ) );
+    }
+
+    public function form( $instance )
+    {
+        $instance = wp_parse_args(
+            (array) $instance
+        );
+
+        if ( isset( $instance['title'] ) )
+        {
+            $title = $instance['title'];
+        }
+        else
+        {
+            $title = __( 'Pořadí', 'wptuts-countdowner-locale' );
+        }
+
+        if ( isset( $instance['seasonId'] ) )
+        {
+            $seasonId = $instance['seasonId'];
+        }
+        else
+        {
+            $seasonId = "";
+        }
+
+        if ( isset( $instance['teamsId'] ) )
+        {
+            $teamsId = $instance['teamsId'];
+        }
+        else
+        {
+            $teamsId = array();
+        }
+
+        if ( isset( $instance['countTeamsAround'] ) )
+        {
+            $countTeamsAround = $instance['countTeamsAround'];
+        }
+        else
+        {
+            $countTeamsAround = -1;
+        }
+
+        // Display the admin form
+        include( plugin_dir_path( __FILE__ ) . '/views/admin-positions.php' );
+    }
+
+    public function update( $new_instance, $old_instance )
+    {
+        $instance = $old_instance;
+
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        $instance['seasonId'] = strip_tags( $new_instance['seasonId'] );
+        $instance['teamsId'] = esc_sql( $new_instance['teamsId'] );
+        $instance['countTeamsAround'] = strip_tags( $new_instance['countTeamsAround'] );
+        return $instance;
+    }
+
+    public function widget( $args, $instance )
+    {
+        extract( $args, EXTR_SKIP );
+
+        echo $before_widget;
+
+        $title = apply_filters('widget_title', $instance['title']);
+        $seasonId = $instance['seasonId'];
+        if(isset($instance['countTeamsAround']))
+        {
+            $countTeamsAround = $instance['countTeamsAround'];
+        }
+
+        //positions
+        $url = URL . "positions-in-season/" . $seasonId;
+        if( is_array( $instance['teamsId'] ) )
+        {
+            $url.= "?team=" . implode( "x", $instance['teamsId'] );
+        }
+
+        if(isset($countTeamsAround))
+        {
+            $url .= "&countTeamsAround=" . $countTeamsAround;
+        }
+
+        $json = file_get_contents( $url );
+        $data = json_decode( $json, TRUE );
+
+        //positions
+        $table = "<table class='nastrikej'>
+                    <tr>
+                        <th>Pořadí</th>
+                        <th>Družstvo</th>
+                        <th>Body</th>
+                    </tr>";
+
+        foreach( $data["positions"] as $aRowPosition )
+        {
+            $strongStartTag = "";
+            $strongEndTag = "";
+            if($aRowPosition['strong'] === TRUE)
+            {
+                $strongStartTag = "<strong>";
+                $strongEndTag = "</strong>";
+            }
+            $table.= "<tr>
+                            <td>{$strongStartTag}{$aRowPosition['position']}{$strongEndTag}.</td>
+                            <td><a href='{$aRowPosition['team_href']}' target='_blank'>{$strongStartTag}{$aRowPosition['team']}{$strongEndTag}</a></td>
+                            <td>{$strongStartTag}{$aRowPosition['points']}{$strongEndTag}</td>
+                        </tr>";
+        }
+
+        $table.= "</table>";
+
+        include( plugin_dir_path( __FILE__ ) . '/views/widget.php' );
+
+        echo $after_widget;
+    }
+
+}
+
+
+add_action('widgets_init', create_function('', 'return register_widget("nastrikejCompetitions");'));
+add_action('widgets_init', create_function('', 'return register_widget("nastrikejPositions");'));
 
 ?>
---
+++ views/admin-competitions.php
@@ -0,0 +1,66 @@
+<?php
+$hrefSeasons = "http://nastrikej.cz/json/wp"; //seznam roku a prislusnych lig   
+?>
+
+
+<p>
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Titulek:' ); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'seasonId' ); ?>"><?php _e( 'Sezóna:' ); ?></label>
+    <?php
+    $json = file_get_contents( $hrefSeasons );
+    $jsonData = json_decode( $json, TRUE );
+
+    $seasons = $jsonData["seasons"];
+    ?>    
+    <select id="<?php echo $this->get_field_id( 'seasonId' ); ?>" name="<?php echo $this->get_field_name( 'seasonId' ); ?>">
+        <?php
+        foreach ( $seasons as $year => $competitions )
+        {
+            echo "<optgroup label=\"{$year}\">";
+            foreach ( $competitions as $competition )
+            {
+                echo "<option value=\"{$competition["season_id"]}\"";
+
+                if ( $seasonId == $competition["season_id"] )
+                {
+                    echo " selected=\"selected\"";
+                }
+                echo ">{$competition["name"]} - {$year}</option>";
+            }
+            echo "</optgroup>";
+        }
+        ?>
+
+    </select>
+    <?php //echo "*".esc_attr( $seasonId )."*"; ?>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'teamsId' ); ?>"><?php _e( 'Výsledky týmů:' ); ?></label>
+    <?php
+    $teams = $jsonData["teams"];
+    ?>    
+    <select id="<?php echo $this->get_field_id( 'teamsId' ); ?>" name="<?php echo $this->get_field_name( 'teamsId' ); ?>[]" multiple style="height: 400px;">
+        <?php
+        foreach ( $teams as $name => $teamsCategory )
+        {
+            echo "<optgroup label=\"{$name}\">";
+            foreach ( $teamsCategory as $team )
+            {
+                echo "<option value=\"{$team["team_id"]}\"";
+
+                if ( in_array( $team["team_id"], $teamsId ) )
+                {
+                    echo " selected=\"selected\"";
+                }
+                echo ">{$team["name"]} - {$name}</option>";
+            }
+            echo "</optgroup>";
+        }
+        ?>
+
+    </select>
+
+</p>
